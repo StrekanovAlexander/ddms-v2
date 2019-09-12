514be2e3a1fb10f1c384c4c108f9206f4de64e9b
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-12 13:14:22 +0300

    rebuilded: TeacherController

--- private/Controllers/TeacherController.php
+++ private/Controllers/TeacherController.php
@@ -2,13 +2,27 @@
 namespace App\Controllers;
 
 use App\Common\Pages;
+use App\Models\Teacher;
 
 class TeacherController extends Controller {
 
   public function teachers($request, $response) {
+    $teachers = Teacher::where('is_actual', true)->orderBy('rank')->get()->toArray();
     return $this->view->render($response, 'guest/teacher/index.twig', [
-      'breadcrumbs' => \App\Common\Pages::breadcrumbs([
-        ['Teachers'],
+      'teachers' => Pages::pagination($teachers, $request->getParam('page', 1), 16),
+      'breadcrumbs' => Pages::breadcrumbs([
+        ['Викладачи'],
+      ]),
+    ]);
+  }
+
+  public function teacher($request, $response, $args) {
+    $teacher = Teacher::where('is_actual', true)->find($args['id']);
+    return $this->view->render($response, 'guest/teacher/details.twig', [
+      'teacher' => $teacher,
+      'breadcrumbs' => Pages::breadcrumbs([
+        ['Викладачи', 'teachers'],
+        [$teacher->full_name],
       ]),
     ]);
   }
