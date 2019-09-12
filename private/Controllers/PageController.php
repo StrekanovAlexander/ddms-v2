bf2877885de420833557131a8696b3da0b6fb1fc
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-12 14:53:11 +0300

    rebuilded: PageController

--- private/Controllers/PageController.php
+++ private/Controllers/PageController.php
@@ -9,4 +9,12 @@ class PageController extends Controller {
     ]);
   }
 
+  public function dashboard($request, $response) {
+    return $this->view->render($response, 'admin/dashboard.twig', [
+      'breadcrumbs' => \App\Common\Pages::breadcrumbs([
+        ['Панель керування']
+      ])
+    ]);
+  }
+
 }
