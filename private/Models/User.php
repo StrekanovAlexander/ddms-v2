3d56164862773b4e8ba1b51f2feacf3e6ba57cfc
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-12 14:53:48 +0300

    rebuilded: User model

--- private/Models/User.php
+++ private/Models/User.php
@@ -10,14 +10,12 @@ class User extends Model {
   protected $fillable = [
     'username',
     'password',
-    'email',
   ];
 
   public static function add($array) {
     User::create([
       'username' => $array['username'],
       'password' => password_hash($array['password'], PASSWORD_DEFAULT),
-      'email' => $array['email'],
     ]);
   }
 
