d694e5f14a91cb281fa8a3227441504ae9d91475
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-12 13:14:56 +0300

    rebuilded: Teacher model

--- private/Models/Teacher.php
+++ private/Models/Teacher.php
@@ -5,6 +5,10 @@ use \Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model {
 
-  protected $table = 'ddms_teachers';
+  protected $table = 'teachers';
+
+  public function department() {
+    return $this->belongsTo('App\Models\Department', 'department_id'); 
+  }
 
 }
