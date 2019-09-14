e5f48c1059f5883ee2823dfcb5802372f72c481d
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-14 08:24:12 +0300

    rebuilded: PageController

--- private/Controllers/PageController.php
+++ private/Controllers/PageController.php
@@ -1,11 +1,14 @@
 <?php
 namespace App\Controllers;
 
+use \App\Models\Carousel;
+
 class PageController extends Controller {
 
   public function home($request, $response) {
     return $this->view->render($response, 'guest/home.twig', [
       'activePage' => 'home',
+      'carousel' => Carousel::where('is_actual', true)->get(),
       'breadcrumbs' => \App\Common\Pages::breadcrumbs()
     ]);
   }
