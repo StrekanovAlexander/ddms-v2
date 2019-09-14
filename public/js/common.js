df32de8e23f9a98318f4b3a87f4180921c238a13
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-14 12:57:58 +0300

    rebuilded: common.js

--- public/js/common.js
+++ public/js/common.js
@@ -1,6 +1,13 @@
 ;
 (function() {
 
+  document.addEventListener('DOMContentLoaded', function() {
+    new SmartPhoto(".smart-photo", {
+      nav: false,
+      resizeStyle: 'fit',
+    });
+  });
+
   const alert = document.querySelector('.alert');
   if (alert) {
     alert.querySelector('.fas').addEventListener('click', function() {
