db7ed40610394d8e9b45cca852f600f771b4bf78
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-19 17:03:58 +0300

    rebuilded: Student model

--- private/Models/Student.php
+++ private/Models/Student.php
@@ -6,8 +6,32 @@ use \Illuminate\Database\Eloquent\Model;
 class Student extends Model {
   protected $table = 'students';
 
-  public function department() {
+  protected $fillable = [
+    'section_id',
+    'full_name',
+    'content',
+    'final_year',
+    'slug',
+    'is_actual',
+    'user_id',
+  ];
+
+  public function section() {
     return $this->belongsTo('App\Models\Section', 'section_id'); 
+  }
+
+  public function user() {
+    return $this->belongsTo('App\Models\User', 'user_id'); 
+  }
+
+  public static function students() {
+    return self::query()
+      ->join('sections', 'students.section_id', '=', 'sections.id')
+      ->orderBy('id', 'DESC')
+      ->get([
+        'students.*',
+        'sections.title as section_title',
+      ]);
   }
 
 }
